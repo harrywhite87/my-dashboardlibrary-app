167fc6c2ef35793663c80ae06914b418f9bd1ee2
unknown <ishara.dilshan.it@gmail.com>
2022-02-08 14:17:30 +0530

    line-map-chartpage-update1

--- src/data/product_sale_data.ts
+++ src/data/product_sale_data.ts
@@ -1,8 +1,8 @@
 import { ProductSale } from "src/app/models/product_sale";
 
 export const StatsPieChart: ProductSale[] = [
-    {product: 'Bags & Packages', value: 5},
-    {product: 'Furniture', value: 15},
-    {product: 'Electronics', value: 60},
+    {product: 'Bags & Packages', value: 15},
+    {product: 'Furniture', value: 25},
+    {product: 'Electronics', value: 50},
     {product: 'Accessories', value: 10},
 ];
